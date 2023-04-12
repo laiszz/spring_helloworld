a00af8d9cff877bdd9bbd6514b0bb4a28435e360
Lais <lais.salesms@gmail.com>
2023-04-12 11:58:38 -0300

    Atividade

--- src/main/java/com/generation/helloworld/controller/HelloController.java
+++ src/main/java/com/generation/helloworld/controller/HelloController.java
@@ -9,11 +9,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 	@GetMapping
 	public String hello() {
-		return "<b>Hello Turma 62!</b>";
+		return "<b>Hello World!</b>";
 	}
 	
-	@GetMapping("/nome")
-	public String meuNome() {
-		return "<b>Laís Sales</b>";
+	@GetMapping("/bsm")
+	public String bsm() {
+		return "<b>Responsabilidade Pessoal<br />"
+				+ "Mentalidade de Crescimento<br />"
+				+ "Orientação ao Futuro<br />"
+				+ "Persistência<br />"
+				+ "Comunicação<br />"
+				+ "Orientação aos Detalhes<br />"
+				+ "Trabalho em Equipe<br />"
+				+ "Proatividade</b>";
+	}
+	
+	@GetMapping("/objetivos")
+	public String objetivos() {
+		return "<b>Objetivos<br />"
+				+ "- Reaprender um pouco de html<br />"
+				+ "- Apender o spring<br />"
+				+ "- Colocar o mysql mais em prática";
 	}
 }
